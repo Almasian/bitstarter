7c00c4d95b84e2efe30c7b5ee0cc85d5503d7030
Almasian <nuralmasian@gmail.com>
2013-07-21 17:57:52 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-
+buf = new buffer(128);
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  var k = fs.readFileSync(./bitstarter/index.html);
-  response.send(k.toString('utf8'));
+  buf = fs.readFileSync(./bitstarter/index.html);
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
