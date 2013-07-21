a0bd02432d1138e7af339875770f2884045442d2
Almasian <nuralmasian@gmail.com>
2013-07-21 17:27:06 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var k = fs.readFileSync(./bitstarter/index.html);
+  response.send(k);
 });
 
 var port = process.env.PORT || 5000;
